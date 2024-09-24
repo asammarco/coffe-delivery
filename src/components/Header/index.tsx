2ad914bfc82a5d7432cfff1f6fffea31518d4f49
asammarco <sammarco.andre@gmail.com>
2024-09-24 20:16:44 -0300

    add Home and Checkout pages and routes

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <h1>Header - Coffee Delivery</h1>
+    </HeaderContainer>
+  )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { HomeContainer } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <h1>Home - Coffee Delivery</h1>
+    </HomeContainer>
+  )
+}
